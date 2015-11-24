ea3bdb340ba4c638b1f5637308bce26ea62323cb
Alex Garland <jeffrey.garland@yale.edu>
2015-11-24 13:27:44 -0500

    Added hashed password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, :uniqueness => { :case_sensitive => false}, format: {with: EMAIL_FORMAT}
+  has_secure_password
+  validates :password, presence: true
 end
