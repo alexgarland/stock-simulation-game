20c1b89541c5f2076aebd14e9586984c484b86f6
Alex Garland <jeffrey.garland@yale.edu>
2015-11-24 13:17:51 -0500

    Changed schema to work with has_secure_password.

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20151124170305) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "hash_pass"
+    t.string   "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
