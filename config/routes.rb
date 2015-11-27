589acfc639be49b67f5cc5f4b9eb32a1cd04679c
Alex Garland <jeffrey.garland@yale.edu>
2015-11-26 20:43:13 -0500

    Added infrastructure for logging in and remembering session.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#home'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resource :users
 end
