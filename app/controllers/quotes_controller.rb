130ccd6d9e58fc4b59c68e3d77cebc60545fdb84
Alex Garland <jeffrey.garland@yale.edu>
2015-11-28 14:16:39 -0500

    Basic quote infrastructure is working now.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,6 +3,8 @@ class QuotesController < ApplicationController
   end
 
   def get_quote
-    render new
+    yahoo_client = YahooFinance::Client.new
+    @stock_symbol = params[:symbol]
+    @data = yahoo_client.quotes(params[:symbol], [:last_trade_price])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   root 'welcome#home'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   get 'quotes' => 'quotes#new'
+  post 'quotes' => 'quotes#get_quote'
   resource :users
 end
