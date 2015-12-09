44ba57d21b4a4d4a7cb542bc81cb124c64154b02
Alex Garland <jeffrey.garland@yale.edu>
2015-12-09 01:07:28 -0500

    Finished chartkick implementation.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,6 +9,7 @@ class QuotesController < ApplicationController
     yahoo_client = YahooFinance::Client.new
     @stock_symbol = params[:symbol]
     @data = yahoo_client.quotes(params[:symbol], [:last_trade_price])
+    @historical_data = yahoo_client.historical_quotes(@stock_symbol[0], {raw: false, start_date: Date.today - 365, end_date: Date.today})
   end
 end
 
